06fd2eec7f13dbf9cc522202a94a414bf409b405
RcnonS <ratchanon.somprasong@gmail.com>
2024-06-05 13:26:27 +0700

    refactor: react form handling-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingInputMessage, setGreetingInputMessage] = useState("");
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+
+  const handleInputMessage = (event) => {
+    setGreetingInputMessage(event.target.value);
+  };
+  const handleMessage = () => {
+    setGreetingMessage(greetingInputMessage);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={greetingInputMessage}
+          onChange={handleInputMessage}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleMessage}>Update text</button>
       </div>
     </div>
   );
